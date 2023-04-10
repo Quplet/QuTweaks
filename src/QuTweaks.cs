1888e17c72432aea46ed1e59fbd90c3e6ee574e0
BuildTools <jonathanhart1114@gmail.com>
2023-04-10 14:45:52 -0400

    initial commit

---
+++ src/QuTweaks.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Security.Permissions;
+using BepInEx;
+using Mono.Cecil.Cil;
+using UnityEngine;
+//using Mono.Cecil.Cil;
+using MonoMod.Cil;
+
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+namespace QuTweaks
+{
+    [BepInPlugin("qu.qu_tweaks", "QuTweaks", "0.1.0")]
+    public class QuTweaks : BaseUnityPlugin
+    {
+        public const string ModID = "qu.qu_tweaks";
+        private readonly QuTweaksOptions _options;
+
+        public QuTweaks()
+        {
+            _options = new QuTweaksOptions();
+        }
+        
+        private void OnEnable()
+        {
+            On.RainWorld.OnModsInit += RainWorldOnModsInitHook;
+            On.LizardTongue.Update += LizardTongueUpdateHook;
+            IL.DropBug.FlyingWeapon += DropBugFlyingWeaponInject;
+            On.Lizard.Violence += LizardViolenceHook;
+        }
+
+        // For the Easier tongue break option
+        private void LizardTongueUpdateHook(On.LizardTongue.orig_Update orig, LizardTongue self)
+        {
+            var oldDist = self.dist;
+            
+            // Original method call
+            orig(self);
+            
+            if (!_options.EasierTongueBreak.Value || !self.StuckToSomething ||
+                !self.lizard.AI.DoIWantToHoldThisWithMyTongue(self.attached) || self.stuckCounter <= 5) return;
+            
+            var dDist = self.dist - oldDist;
+            //Debug.Log($"oldDist: {oldDist}, newDist: {self.dist}, dDist: {dDist}, stuckCounter: {self.stuckCounter}");
+            
+            if (dDist < 5f - 2f * Mathf.InverseLerp(20f, 200f, self.stuckCounter)) return;
+            
+            //Debug.Log("Released!");
+            self.Retract();
+        }
+
+        // For less trigger happy dropwigs options
+        private void DropBugFlyingWeaponInject(ILContext il)
+        {
+            try
+            {
+                var c = new ILCursor(il);
+                
+                c.GotoNext(
+                    x => x.MatchLdfld<BodyChunk>("pos"),
+                    x => x.MatchLdcR4(1),
+                    x => x.MatchCallOrCallvirt<ArtificialIntelligence>("VisualContact")
+                );
+                c.Index += 4;
+                
+                c.EmitDelegate<Func<bool>>(() => !_options.LessTriggerHappyDropwigs.Value);
+                
+                var returnLabel = c.DefineLabel();
+                c.MarkLabel(returnLabel);
+                
+                c.GotoNext(
+                    x => x.MatchLdarg(1), 
+                    x => x.MatchCallOrCallvirt<PhysicalObject>("get_firstChunk")
+                );
+                var pointToLabel = c.DefineLabel();
+                c.MarkLabel(pointToLabel);
+                
+                c.GotoLabel(returnLabel);
+                c.Emit(OpCodes.Brfalse_S, pointToLabel);
+                
+                /* Testing lines
+                c.Index += 3;
+
+                c.EmitDelegate<Action>(() =>
+                {
+                    Debug.Log("Dropped!");
+                });
+                */
+                //Console.WriteLine(il);
+                
+            }
+            catch (Exception ex)
+            {
+                Debug.LogError("Exception when matching IL for DropBugFlyingWeaponInject!");
+                Debug.LogException(ex);
+                Debug.LogError(il);
+            }
+        }
+
+        // For Heavier spears option
+        private void LizardViolenceHook(
+            On.Lizard.orig_Violence orig,
+            Lizard self,
+            BodyChunk source,
+            Vector2? directionAndMomentum,
+            BodyChunk hitChunk,
+            PhysicalObject.Appendage.Pos onAppendagePos,
+            Creature.DamageType type,
+            float damage,
+            float stunBonus
+        )
+        {
+            // Condition check
+            var bl = _options.HeavierSpears.Value && directionAndMomentum.HasValue &&
+                     LizardHitHeadShield(self, directionAndMomentum.Value) && source?.owner is Spear && 
+                     self.Template.type != CreatureTemplate.Type.RedLizard;
+
+            // Sets the stun equal to a rock
+            if (bl)
+            {
+                stunBonus = 45;
+                if (ModManager.MSC && source?.owner != null &&
+                    source.owner.room.game.IsArenaSession && source.owner.room.game.GetArenaGameSession.chMeta != null)
+                    stunBonus = 90;
+            }
+            
+            // Original method call
+            orig(self, source, directionAndMomentum, hitChunk, onAppendagePos, type, damage, stunBonus);
+            
+            if (!bl || source == null) return;
+            
+            // Flip lizziboi
+            self.turnedByRockDirection = (int) Mathf.Sign(source.pos.x - source.lastPos.x);
+            self.turnedByRockCounter = 20;
+        }
+
+        // For registering the options menu
+        private void RainWorldOnModsInitHook(On.RainWorld.orig_OnModsInit orig, RainWorld self)
+        {
+            orig(self);
+            MachineConnector.SetRegisteredOI(ModID, _options);
+        }
+
+        // Recreation of the Lizard HitHeadShield method without the sound method involved
+        private static bool LizardHitHeadShield(Lizard lizard, Vector2 direction)
+        {
+            var hitAngle = Vector2.Angle(direction, -lizard.bodyChunks[0].Rotation);
+            var maxLizardHeadShieldAngle = lizard.lizardParams.headShieldAngle + 20.0 * lizard.JawOpen;
+            return !(lizard.HitInMouth(direction) || hitAngle >= maxLizardHeadShieldAngle);
+        }
+    }
+}
---
+++ src/QuTweaksOptions.cs
@@ -0,0 +1,83 @@
+﻿using Menu.Remix.MixedUI;
+
+namespace QuTweaks
+{
+    public class QuTweaksOptions : OptionInterface
+    {
+        public readonly Configurable<bool> EasierTongueBreak;
+        public readonly Configurable<bool> LessTriggerHappyDropwigs;
+        public readonly Configurable<bool> HeavierSpears;
+
+        public QuTweaksOptions()
+        {
+            EasierTongueBreak = config.Bind(nameof(EasierTongueBreak), true);
+            LessTriggerHappyDropwigs = config.Bind(nameof(LessTriggerHappyDropwigs), true);
+            HeavierSpears = config.Bind(nameof(HeavierSpears), false);
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+            
+            var opTab = new OpTab(this, "Options");
+            Tabs = new[] { opTab };
+            var titleBox = new OpLabel(16f, 560f, "Configuration", true);
+            
+            /*
+            var easierTongueBreakBox = new OpCheckBox(EasierTongueBreak, 16f, 520f);
+            easierTongueBreakBox.description = 
+                "Makes it easier to break out of certain lizard tongue grips. Default is true.";
+            var easierTongueBreakLabel = new OpLabel(easierTongueBreakBox.PosX + 40f, easierTongueBreakBox.PosY, "Easier lizard tongue break");
+            easierTongueBreakLabel.description = easierTongueBreakBox.description;
+            */
+
+            var easierTongueBreakConfig = new QuConfigOption(new OpCheckBox(EasierTongueBreak, 16f, 520f), 
+                "Easier lizard tongue break", 
+                "Makes it easier to break out of certain lizard tongue grips. Default is true.");
+            
+            /*
+            var lessTriggerHappyDropwigsBox = new OpCheckBox(LessTriggerHappyDropwigs, 16f, 480f);
+            lessTriggerHappyDropwigsBox.description =
+                "Makes dropwigs not instantly drop when you throw a spear at them. Default is true.";
+            var lessTriggerHappyDropwigsLabel = new OpLabel(lessTriggerHappyDropwigsBox.PosX + 40f, lessTriggerHappyDropwigsBox.PosY, "Less trigger happy dropwigs");
+            lessTriggerHappyDropwigsLabel.description = lessTriggerHappyDropwigsBox.description;
+            */
+
+            var lessTriggerHappyDropwigsConfig = new QuConfigOption(new OpCheckBox(LessTriggerHappyDropwigs, 16f, 480f),
+                "Less trigger happy dropwigs",
+                "Makes dropwigs not instantly drop when you throw a spear at them. Default is true.");
+
+            /*
+            var heavierSpearsBox = new OpCheckBox(HeavierSpears, 16f, 440f);
+            heavierSpearsBox.description = 
+                "Causes lizards to flip over upon being hit in the head by a thrown spear. Default is false.";
+            var heavierSpearsLabel = new OpLabel(heavierSpearsBox.PosX + 40f, heavierSpearsBox.PosY, "Heavier spears");
+            heavierSpearsLabel.description = heavierSpearsBox.description;
+            */
+
+            var heavierSpearsConfig = new QuConfigOption(new OpCheckBox(HeavierSpears, 16f, 440f),
+                "Heavier spears",
+                "Causes lizards to flip over upon being hit in the head by a thrown spear. Default is false.");
+            
+            opTab.AddItems(titleBox);
+            opTab.AddItems(easierTongueBreakConfig.Elements);
+            opTab.AddItems(lessTriggerHappyDropwigsConfig.Elements);
+            opTab.AddItems(heavierSpearsConfig.Elements);
+        }
+
+        private class QuConfigOption
+        {
+            public UIelement[] Elements { get; }
+
+            public QuConfigOption(UIconfig config, string name, string desc)
+            {
+                config.description = desc;
+                var label = new OpLabel(config.PosX + config.size.x + 20f, config.PosY, name)
+                {
+                    description = desc
+                };
+                Elements = new UIelement[] { config, label };
+            }
+        }
+    }
+}
